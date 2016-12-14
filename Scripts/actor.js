67d1e00e4e1fc9b637afb75570f20b07f3650939
liderb <migspyderman@hotmail.com>
2016-12-14 00:28:45 +0000

    Restyle
    Modificado layout para dispositivos com altura inferior a 30%.
    Adicionado placeholders para diretores

--- Scripts/actor.js
+++ Scripts/actor.js
@@ -12,12 +12,14 @@ $("document").ready(function() {
 			var final_link = api_base + actorID;
 			console.log(final_link);
 			$.getJSON(final_link, function(data) {
+				if (data[0].photo === '/images/nophoto.png') {
+					data[0].photo = './images/nophoto.png';
+				};
+				data[0].actorFacebookLikes = data[0].actorFacebookLikes + " <i class='fa fa-thumbs-o-up' aria-hidden='true'></i>"
 				self.actor(data);
 				console.log(data);
 		})};
-
 		getActor();
-		console.log(self.actor);
 	}
 	ko.applyBindings(viewActors);
 });
---
+++ Scripts/director.js
@@ -0,0 +1,25 @@
+$("document").ready(function() {
+	var location = window.location.href.split("#");
+	var api_base = "http://192.168.160.39/api/Directors/";
+	var viewDirectors = new function(){
+		var self = this;
+		var directorID = location[1];
+		console.log(directorID);
+
+		self.director = ko.observableArray();
+
+		getDirector = function () {
+			var final_link = api_base + directorID;
+			console.log(final_link);
+			$.getJSON(final_link, function(data) {
+				if (data[0].photo === '/images/nophoto.png') {
+					data[0].photo = './images/nophoto.png';
+				};
+				data[0].directorFacebookLikes = data[0].directorFacebookLikes + " <i class='fa fa-thumbs-o-up' aria-hidden='true'></i>"
+				self.director(data);
+				console.log(data);
+		})};
+		getDirector();
+	}
+	ko.applyBindings(viewDirectors);
+});
